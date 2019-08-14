9ef1e82ea1032b3e7f2c461745e4732912f7cb70
Liam Dawson <liamrdawson@gmail.com>
2019-08-14 21:59:17 +0100

    update rating state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Course from './Course';
 
 const App = (props) => {
+
   return (
     <div className="container">
       {props.courses.map( course => 
         <Course
           {...course}
           key={course.id}
+
         />      
       )}
     </div>
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -14,7 +14,9 @@ class StarRating extends Component {
     for (let i = 0; i < 5; i++) {
       starArray.push(
         <Star 
-          key={i}/>
+          key={i}
+          setRating={ () => this.handleChangeRating(i+1) }  
+          />
       );
     }
     return starArray;
@@ -23,6 +25,12 @@ class StarRating extends Component {
   // Write an event handler that updates the rating state.
   // Pass the function to a Star component via props
 
+  handleChangeRating = (rating) => {
+    this.setState({
+      rating: rating
+    });
+    console.log(rating);
+  }
 
   render() {
     return (
